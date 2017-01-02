603ce880ac2576dae7841497b61e91b95daa9769
karellincoln <2218374291@qq.com>
2017-01-02 10:00:25 +0800

    为了测试实验数据做的准备

---
+++ random_trace_3D/make_bench.py
@@ -0,0 +1,20 @@
+import os
+import random
+os.system('rm -f bench')
+
+num = int(raw_input("input the num of the trace: "))
+
+
+bench="./bench"
+file1=open(bench,"w")
+
+i=0
+time = 0.0004
+while i<num:
+#  trace = str(time) + " "+str(random.randrange(16))+" "+str(random.randrange(16))+" "+str(random.randrange(3))+" "+str(random.randrange(16))+" "+str(random.randrange(16))+" "+str(random.randrange(3))+" "+str(5)+"\n"
+  trace = "0.1 0 0 0 0 0 1 10\n"
+  time = time + random.random()
+  i = i+1
+  file1.write(trace)
+
+file1.close()
